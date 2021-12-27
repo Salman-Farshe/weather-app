acb8d19b2198cce1fcc5f5bce93ffc96c2bec830
Salman Farshe <salmanfarshe39@gmail.com>
2021-12-27 17:06:23 +0600

    setup project files

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express')
+    app = express()
+    port = 3000
+    bodyParser = require('body-parser')
+    favicon = require('serve-favicon')
+    path = require('path')
+    
+    app.set('view engine', 'ejs') // dont't need to use ejs extention
+    
+    app.use(express.static('public')) // tell express to serve the content of the public directory
+    app.use(bodyParser.urlencoded({extended: true})) // retrive data from the body
+    app.use(favicon(path.join(__dirname, 'public', 'favicon.ico'))) // allowing tha path for favicon 
+    
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+app.listen(port, () => {
+    console.log('server starting')
+})
+
+/*
+
+// changes views directory & rename it....
+const viewsPath = path.join(__dirname, '../templates/views')  // views path changes
+app.set('views', viewsPath)
+
+
+const publicDirectory = path.join(__dirname, '../public')
+// customize server, serve out that folder
+// setup static directory
+app.use(express.static(publicDirectory))
+
+*/
