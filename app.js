3799ed24b32bf63bcf941bb90ba25d50cdf5a000
Salman Farshe <salmanfarshe39@gmail.com>
2021-12-27 23:31:37 +0600

    adding header section

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
-const express = require('express')
-    app = express()
-    port = 3000
-    bodyParser = require('body-parser')
-    favicon = require('serve-favicon')
-    path = require('path')
+const express = require('express'),
+    app = express(),
+    port = 3000,
+    bodyParser = require('body-parser'),
+    favicon = require('serve-favicon'),
+    path = require('path');
     
     app.set('view engine', 'ejs') // dont't need to use ejs extention
     
     app.use(express.static('public')) // tell express to serve the content of the public directory
     app.use(bodyParser.urlencoded({extended: true})) // retrive data from the body
     app.use(favicon(path.join(__dirname, 'public', 'favicon.ico'))) // allowing tha path for favicon 
-    
+    app.use(express.static(path.join(__dirname, 'img'))) // serve the another static directory
+
 app.get('/', (req, res) => {
     res.render('index')
 })
